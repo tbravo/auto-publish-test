87f9de7354d912ccc3a82f19a243dae259bbf097
tbravo <43784645+tbravo@users.noreply.github.com>
2020-04-21 09:10:06 -0500

    gitignore dist files

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-var _otherFile = _interopRequireDefault(require("./other-file.js"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-
-console.log(_otherFile["default"]);
